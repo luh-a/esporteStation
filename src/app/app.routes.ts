4f71e47acbaf78679a598f1168f2db6e680c7bbf
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-08-20 19:21:22 -0300

    Inserção de rotas funcionais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home-component/home-component';
+import { AtletaComponent } from './component/atleta-component/atleta-component';
+import { CorridaComponent } from './component/corrida-component/corrida-component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    redirectTo: '/home',
+    pathMatch: 'full'
   },
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'cadastroAtleta',
+    component: AtletaComponent
+  },
+  {
+    path: 'cadastroCorrida',
+    component: CorridaComponent
   }
 ];
--- src/app/component/menu-component/menu-component.ts
+++ src/app/component/menu-component/menu-component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-menu-component',
   styleUrl: './menu-component.css',
   templateUrl: './menu-component.html',
