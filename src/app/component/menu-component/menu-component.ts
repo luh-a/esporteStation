84cc294ab3437f46d66aca7d34727267899ada85
Anna Luz <anna.mecenas@aluno.senai.com.br>
2026-08-19 21:06:59 -0300

    Criação e vinculação do componente Menu

---
+++ src/app/component/menu-component/menu-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-menu-component',
+  styleUrl: './menu-component.css',
+  templateUrl: './menu-component.html',
+})
+export class MenuComponent {}
