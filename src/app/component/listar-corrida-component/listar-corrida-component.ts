031f5dbc294a3a4822e0c8537c950e0bae7fac5b
Anna Luz <anna.mecenas@aluno.senai.br>
2026-08-24 19:41:34 -0300

    criação do conteudo de listar corridas.ts

--- src/app/component/listar-corrida-component/listar-corrida-component.ts
+++ src/app/component/listar-corrida-component/listar-corrida-component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { CorridaService } from '../../services/corrida-service';
+import { Corrida } from '../../models/corridaModel';
+
 
 @Component({
-  imports: [],
+  imports: [ CommonModule ],
   selector: 'app-listar-corrida-component',
   styleUrl: './listar-corrida-component.css',
   templateUrl: './listar-corrida-component.html',
 })
-export class ListarCorridaComponent {}
+export class ListarCorridaComponent implements OnInit {
+  //signal = manda um sinal atualizando a lista de corridas
+  corridas = signal<Corrida[]>([]);
+
+  // imports -> variáveis
+  constructor(private corridaService: CorridaService, private router: Router) {}
+
+  ngOnInit() {
+    this.atualizarCorridas();
+  }
+
+  editarCorrida(idCorrida: number) {
+    this.router.navigate(['/cadastroCorrida', idCorrida]);
+  }
+
+  deletarCorrida(idCorrida: number) {
+    //pega a função remover do service
+    this.corridaService.remover(idCorrida).subscribe(() => {
+      this.atualizarCorridas();
+    });
+  }
+
+  private atualizarCorridas() {
+    this.corridaService.listar().subscribe((informacoes) => {
+      this.corridas.set(informacoes);
+    });
+  }
+
+}
