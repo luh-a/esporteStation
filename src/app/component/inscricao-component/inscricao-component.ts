44de1dcaa6fb21f12dedee81be47eb51e4bc742b
Anna Luz <anna.mecenas@aluno.senai.br>
2026-08-24 21:43:02 -0300

    criação dos componentes, service e model de inscrição corrida

---
+++ src/app/component/inscricao-component/inscricao-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-inscricao-component',
+  styleUrl: './inscricao-component.css',
+  templateUrl: './inscricao-component.html',
+})
+export class InscricaoComponent {}
---
+++ src/app/models/inscricaoModel.ts
@@ -0,0 +1,6 @@
+export class inscricao {
+  idInscricao: number = 0.0 ;
+  idAtleta: number = 0.0 ;
+  idCorrida: number = 0.0 ;
+  camisaTam: string = '' ;
+}
